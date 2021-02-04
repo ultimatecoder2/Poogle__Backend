f45336b1c3d3bb7647030379908dd4fe6bd68e9d
bhavesh <bkpassi2017@gmail.com>
2021-02-05 01:26:25 +0530

    issue resolved

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const upload = multer({
 	dest: "images",
 });
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/routers/user.js
+++ src/routers/user.js
@@ -20,9 +20,11 @@ router.post("/users", async (req, res) => {
 
 //Login
 router.post('/users/login',async(req,res)=>{
+
     try{
         const user = await User.findByCredentials(req.body.email,req.body.password);
-        const token = await user.generateAuthToken()
+        const token = await user.generateAuthToken();
+
         res.send({user,token})
     }
     catch(e){
