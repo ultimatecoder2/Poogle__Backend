b1ac2609aa43160987592a1b3289489af891a606
bhavesh <bkpassi2017@gmail.com>
2021-02-05 00:42:04 +0530

    bug fixed

--- src/index.js
+++ src/index.js
@@ -23,14 +23,14 @@ const upload = multer({
 	dest: "images",
 });
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //app.use(express.static(path.join(__dirname, '../public')));
 app.use("/uploads", express.static("uploads"));
 
-app.use(cors());
 app.use(userRouter);
 app.use(spaceRouter);
 app.use(questionRouter);
@@ -42,7 +42,7 @@ app.use(uploadRouter);
 app.use(chatRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
 	next(new Error(404));
 });
 
@@ -58,6 +58,23 @@ app.use(function (err, req, res, next) {
 	// render the error page
 	res.status(err.status || 500);
 	res.json({ error: err });
+});*/
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+  });
+  
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.json({ error: err })
+	
 });
 
 app.listen(port, () => {
