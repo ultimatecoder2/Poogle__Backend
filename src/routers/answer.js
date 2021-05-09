ae2cef4f5126349c6f9af00a16756ce9d80d7ee0
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-05-09 16:17:04 +0530

    updated answer api

--- src/routers/answer.js
+++ src/routers/answer.js
@@ -133,11 +133,21 @@ answerRouter.get("/useranswers", auth, async (req, res) => {
         let skip = Skip?parseInt(Skip):0;
         let answers;
         if(userId){
-		    answers = await Answers.find({"author":userId}).populate('author').sort({"updatedAt":-1}).skip(skip).limit(limit);
+		    answers = await Answers.find({"author":userId}).populate('author').populate('question').sort({"updatedAt":-1}).skip(skip).limit(limit);
         }else{
-            answers = await Answers.find({}).populate('author').sort({"updatedAt":-1}).skip(skip).limit(limit);
+            answers = await Answers.find({}).populate('author').populate('question').sort({"updatedAt":-1}).skip(skip).limit(limit);
 		}
-        res.status(200).send(answers);
+        let myans = answers;
+        for(var i=0;i<myans.length;i+=1){
+            let author = myans[i].author;
+            
+            author = {"_id":author._id, "name":author.name,"user_name":author.user_name}
+            let ques = myans[i].question;
+            ques = {"_id":ques._id, "heading": ques.heading}
+            myans[i].question = ques;
+            myans[i].author = author;
+        }
+        res.status(200).send(myans);
 	} catch (e) {
 		console.log(e);
 		res.status(500).send();
